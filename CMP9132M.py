cb2ebac22101376f7ee1ef0e2ed66482a58eac96
Benjamin Wilson <12446446@students.lincoln.ac.uk>
2019-12-04 12:04:29 +0000

    finished 1a, start of 1b

--- CMP9132M.py
+++ CMP9132M.py
@@ -1,6 +1,8 @@
+import csv
+#1a
 def probOfOne(value):
     disease = 1.0/float(value)
-    print("The probability of having the disease is " + str(disease))
+    #print("The probability of having the disease is " + str(disease))
     return disease
 
 def probAnB(a, b):
@@ -9,18 +11,49 @@ def probAnB(a, b):
 def probAgivenB(a, b):
     return probAnB(a, b) / b
 
+def notAB(a, b):
+    na = 1 - a
+    nb = 1 - b
+    return probAgivenB(na, nb)
+
 def diseaseProbability():
     print("Please input the probability of the disease being true")
     d = input()
     print("P(t|d) = " + str(probAgivenB(0.99, probOfOne(d))))
+    print("P(¬t|¬d) = " + str(notAB(0.99, probOfOne(d))))
 
+
+#1b
+class Node():
+    def __init__(self, n, p, pa, ch):
+        self.name = n
+        self.prob = p
+        self.parents = pa
+        self.children = ch
+    
+
+
+def bayes(filename):
+    with open(filename, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',') 
+        line_count = 0      
+        for row in csv_reader:
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+
+
+    group = [0,0]
+    #group[0] = Node(csv_file["smoking"])
+
+#main
 def main():
     print("Which task do you want to run?")
     i = input()
     if i == "1a":
         diseaseProbability()
     elif i == "1b":
-        pass
+        print("Filename: ")
+        bayes(input())
     elif i == "2":
         pass
     else:
